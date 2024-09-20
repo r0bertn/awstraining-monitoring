b02c18c0d1cb1d09a4d5d81877b57549ddc23468
Robert Nowiński <robert.o.nowinski@gmail.com>
2024-09-20 11:10:42 +0200

    custom log

--- app/src/main/java/com/awstraining/backend/business/measurements/controller/DeviceController.java
+++ app/src/main/java/com/awstraining/backend/business/measurements/controller/DeviceController.java
@@ -44,6 +44,7 @@ class DeviceController implements DeviceIdApi {
                 .toList();
         final Measurements measurementsResult = new Measurements();
         measurementsResult.measurements(measurements);
+        LOGGER.info("Size of measurements: {}", measurements.size());
         return ResponseEntity.ok(measurementsResult);
     }
 
